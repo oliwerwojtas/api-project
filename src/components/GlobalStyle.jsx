65154017de8719e5ebf4a6f719dd533da0fb38ce
Titter00 <olo_wojtas@o2.pl>
2023-03-28 17:15:33 +0200

    style: :art: style GameDetails component which contains game details informations

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -13,6 +13,9 @@ html{
     &::-webkit-scrollbar-thumb{
         background-color: darkgrey;
     }
+    &::-webkit-scrollbar-track {
+        background: white;
+      }
     body{
         font-family: 'Montserrat', sans-serif;
         width: 100%;
@@ -29,7 +32,7 @@ html{
     h3{
         font-size: 1.3rem;
         color: #333;
-        padding: 1.5rem;
+        padding: 1.5rem 0rem;
     }
     p{
         font-size: 1.2rem;
